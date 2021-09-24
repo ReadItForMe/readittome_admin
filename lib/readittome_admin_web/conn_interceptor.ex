68e1bb4743046d1084285f4595bae4067a9b0e16
rybex <tomasz.rybczynski@blockfi.com>
2021-09-24 19:33:11 +0200

    Try to setup http

--- lib/readittome_admin_web/conn_interceptor.ex
+++ lib/readittome_admin_web/conn_interceptor.ex
@@ -1,11 +1,13 @@
 defmodule ReadittomeAdminWeb.ConnInterceptor do
   import Plug.Conn, only: [assign: 3]
 
+  require Logger
+
   def init(default), do: default
 
-  def call(conn, _default) do
-    require IEx
-    IEx.pry
+  def call(conn, default) do
+    Logger.info(default)
+    Logger.info(conn)
     conn
   end
 end
