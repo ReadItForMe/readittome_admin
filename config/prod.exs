a9c058379529241de6d124cace5dc1b93507c018
rybex <tomasz.rybczynski@blockfi.com>
2021-09-24 13:12:25 +0200

    fixed config

--- config/prod.exs
+++ config/prod.exs
@@ -10,6 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :readittome_admin, ReadittomeAdminWeb.Endpoint,
+  http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "readittome-admin.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
