226413c970227b05ff1040541f4723c115aaf43b
rybex <tomasz.rybczynski@blockfi.com>
2021-09-24 13:30:52 +0200

    Allow all

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,8 @@ config :readittome_admin, ReadittomeAdminWeb.Endpoint,
   url: [scheme: "https", host: "readittome-admin.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  check_origin: false
 
 config :readittome_admin, ReadittomeAdmin.Repo,
   adapter: Ecto.Adapters.Postgres,
